89c5d1d956a13cee806501b20ca6ae5696670488
genpeng <617735343@qq.com>
2019-02-27 12:18:54 +0800

    金融

---
+++ app/js/core/slider.vue
@@ -0,0 +1,52 @@
+<template>
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{name:item.href}">
+                    <img :src="item.src" alt="">
+                </router-link>
+            </swiper-slide>
+            <div class="swiper-pagination" v-if="options.pagination"></div>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import { swiper,swiperSlide } from 'vue-awesome-swiper';
+export default {
+    components:{
+        swiper,
+        swiperSlide,
+    },
+    props: {
+        //这个变量是swiper指定的变量
+        options: {
+            type:Object,
+            //参数也是swiper指定的参数
+            default(){
+                return{
+                    autoplay: true,
+                    loop: true,
+                    pagination:{
+                        el:'.swiper-pagination',
+                    },
+                    notNextTick: false,
+                }
+            }
+        },
+        items: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    }
+    
+}
+</script>
+
+
+<style lang="css">
+    @import "~swiper/dist/css/swiper.css";
+
+</style>
